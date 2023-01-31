7daf564f88cfb07c9c2d258bb9bdeaadae4961c6
RobertSchell <robert.schell@codeimmersives.com>
2023-01-31 11:38:14 -0800

    added blogs route

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const logger = require('morgan');
 //set up router for each set of routes (index, user)
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
+const blogsRouter = require('./routes/blogs');
 
 //instantiate the actual express app
 //const port = 3001;
@@ -32,6 +33,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 //we bind (associate) the routers to routes in our application
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/blogs', blogsRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {

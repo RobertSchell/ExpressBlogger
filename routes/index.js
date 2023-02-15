57fc3bfa7a35ea97cb5469f68f68a288b5a3e5f3
RobertSchell <robert.schell@codeimmersives.com>
2023-02-15 12:04:21 -0800

    cleaned up

--- routes/index.js
+++ routes/index.js
@@ -1,22 +1,3 @@
-// var express = require('express');
-// var router = express.Router();
-
-// const Blog = require('./model/Blogs');
-
-// /* GET home page. */
-// router.get('/', async function(req, res) {
-
-//   try {
-//     const allBlogs = await Blog.find({});
-//     res.json({ blogs: allBlogs });
-// }catch(e) {
-//   console.log(e);
-// }
-// });
-
-
-// module.exports = router;
-
 var express = require('express');
 var router = express.Router();
 
--- routes/users.js
+++ routes/users.js
@@ -1,15 +1,3 @@
-// const express = require('express');
-// const router = express.Router();
-
-// /* GET users listing. */
-// router.get('/', function(req, res, next) {
-//   res.send('respond with a resource');
-// });
-
-// module.exports = router;
-
-
-
 const express = require("express");
 const router = express.Router();
 
